9a0836b2b8f3e485eef2de32275962acd8989c10
raziqali0306 <raziqali0306@gmai.com>
2021-12-12 22:30:34 +0530

    Files modified

--- src/common/Api.js
+++
@@ -1,9 +0,0 @@
-class Api {
-  baseUrl = 'https://api.pexels.com/v1/';
-
-  getPics() {} // Get initial pics array
-
-  searchPics() {} // get seach result pics array
-}
-
-export default Api;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Navbar from './components/navbar.jsx';
-import Pics from './components/pics.jsx';
-
 ReactDOM.render(
   <React.StrictMode>
-    <Navbar />
-    <Pics />
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
