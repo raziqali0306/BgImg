e1e4a96e0ec537fa99ae6788d69cdea9d1243b37
raziqali0306 <raziqali0306@gmai.com>
2021-12-26 14:51:01 +0530

    removed router

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
+  </React.StrictMode>,
   document.getElementById('root'),
 );
 
