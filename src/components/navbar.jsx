0d13c4bc0898c82949076484e34245b6c9912258
raziqali0306 <raziqali0306@gmai.com>
2021-12-27 11:59:53 +0530

    fixed mobile view

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -9,10 +9,10 @@ function Navbar(props) {
   return (
     <div>
       {/* navbar */}
-      <nav className='navbar sticky-top navbar-dark bg-primary rounded-bottom px-5 py-3'>
+      <nav className='navbar sticky-top navbar-dark bg-primary rounded-bottom px-4 py-2'>
           <a href='https://bgimg.netlify.app/' className='navbar-brand fw-bolder fs-2' style={{fontFamily: 'Libre Baskerville'}} target={'_blank'} rel="noreferrer">BgImg</a>
           
-          <form className='d-flex' onSubmit={(event) => {event.preventDefault()}}>
+          <form className='d-flex py-2' onSubmit={(event) => {event.preventDefault()}}>
               <input
                 className='form-control me-2'
                 type='text'
@@ -22,12 +22,12 @@ function Navbar(props) {
                 onChange={(e) => {props.updateQuery(e.target.value)}}
               />
               <div className="dropdown col-3">
-                <input type="text" value={props.orientation} className="btn btn-warning dropdown-toggle rounded-3 col-12" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false" />
-                <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
-                  <li className='dropdown-item' onClick={() => {props.updateOrientation('All')}}>All</li>
-                  <li className='dropdown-item' onClick={() => {props.updateOrientation('Landscape')}}>Landscape</li>
-                  <li className='dropdown-item' onClick={() => {props.updateOrientation('Portrait')}}>Potrait</li>
-                  <li className='dropdown-item' onClick={() => {props.updateOrientation('Square')}}>Square</li>
+                <input type="image" value={props.orientation} className="btn btn-warning dropdown-toggle rounded-3 col-12 px-0" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false" />
+                <ul className="dropdown-menu text-center" aria-labelledby="dropdownMenuButton2">
+                  <li className='dropdown-item' onClick={() => {props.updateOrientation('All')}}>All  <img src="https://img.icons8.com/ios-glyphs/24/000000/multiple-devices.png"/></li>
+                  <li className='dropdown-item' onClick={() => {props.updateOrientation('Landscape')}}>Landscape  <img src="https://img.icons8.com/material-outlined/24/000000/laptop.png" /></li>
+                  <li className='dropdown-item' onClick={() => {props.updateOrientation('Portrait')}}>Potrait  <img src="https://img.icons8.com/material-outlined/24/000000/iphone--v2.png"/></li>
+                  <li className='dropdown-item' onClick={() => {props.updateOrientation('Square')}}>Square  <img src="https://img.icons8.com/fluency-systems-regular/24/000000/rounded-rectangle.png"/></li>
                 </ul>
               </div>
             <button type="button" className="btn btn-danger ms-2" id="liveToastBtn" onClick={() => {props.searchPics(true)}}>Search</button>
